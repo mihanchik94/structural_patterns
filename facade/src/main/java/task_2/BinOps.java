7707403c1dd0539979b43224c4d7d8911578ce84
mihanchik94 <mihanchik94@gmail.com>
2022-09-13 20:39:09 +0300

    Двоичный фасад (фасад)

---
+++ facade/src/main/java/task_2/BinOps.java
@@ -0,0 +1,15 @@
+package task_2;
+
+public class BinOps {
+    public String sum(String a, String b) {
+        int first = Integer.parseInt(a, 2);
+        int second = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(first + second);
+    }
+
+    public String mult(String a, String b) {
+        int first = Integer.parseInt(a, 2);
+        int second = Integer.parseInt(b, 2);
+        return Integer.toBinaryString(first * second);
+    }
+}
